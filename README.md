2c9a449a29d29aa96d41660f5433b61b63417b28
nigishi <y.yamamoto88@gmail.com>
2020-07-25 19:21:21 +0900

    update

--- README.md
+++ README.md
@@ -1,3 +1,21 @@
 # README.md
 
-test
+使うツール
+API Gateway
+zookeeper
+kafka
+keepalived
+mysqld
+
+プログラミング言語は何にするか。
+golang
+python3
+rust
+
+golangにしよう。仕様がシンプル。書きやすい。人気もあるのでドキュメントもある。
+contrailもgolangに移行しようとしている。golangにしよう。
+
+コンポーネント群
+controller
+compute
+webui
--- controller.py
+++ controller.py
@@ -1,4 +1,18 @@
-def bgp():
-    print('Hello')
+from datetime import datetime
+import socket
 
-bgp()
+print('The server started at', datetime.now())
+print('Waiting...')
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind('127.0.0.1', 50000)
+    s.listen(1)
+    while True:
+        conn, addr = s.accept()
+        with conn:
+            while True:
+                data = conn.recv(1024)
+                if not data:
+                    break
+                print('data: ', data, 'addr: ', addr)
+                conn.sendall(b'Receive: ' + data)
